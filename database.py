e2a32fa57d3331d482ad7cc614d2201b96a2be90
김윤기 <kalsagansteve@gmail.com>
2025-06-24 20:19:04 +0900

    fix: db setting

--- database.py
+++ database.py
@@ -8,14 +8,14 @@ import dotenv
 dotenv.load_dotenv()
 
 # Retrieve database connection details from environment variables
-DB_USER = os.getenv('USER')
-DB_PASSWORD = os.getenv('PASSWORD')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 HOST = os.getenv('HOST')
 PORT = os.getenv('PORT')
 DBNAME = os.getenv('DBNAME')
 
 # Form the database URL
-DB_URL = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}'
+DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{HOST}:{PORT}/{DBNAME}'
 
 engine = create_engine(DB_URL, connect_args={"connect_timeout": 10}, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
