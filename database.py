59783ff5bf97df26e9952a1dbb537fb1232487dd
김윤기 <kalsagansteve@gmail.com>
2025-06-24 20:28:16 +0900

    fix: db setting

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 import os
 import dotenv
+from sqlmodel import Session, SQLModel
 
 # Load the environment variables
 dotenv.load_dotenv()
@@ -17,7 +19,20 @@ DBNAME = os.getenv('DBNAME')
 # Form the database URL
 DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{HOST}:{PORT}/{DBNAME}'
 
-engine = create_engine(DB_URL, connect_args={"connect_timeout": 10}, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DB_URL,
+    connect_args={"connect_timeout": 10},
+    pool_size=20,          # 기본 풀 크기
+    max_overflow=40,       # 최대 오버플로우 수
+    pool_timeout=30,       # 풀에서 커넥션을 가져오기 위해 대기하는 최대 시간(초)
+    pool_recycle=1800,     # 연결을 재활용하기 전에 대기할 시간(초)
+    pool_pre_ping=True     # 연결이 살아있는지 확인하기 위해 사전 핑을 활성화
+)
 
-Base = declarative_base()
+SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
